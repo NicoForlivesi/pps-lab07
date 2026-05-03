cca5d739bfd541285f016d54329157ea8619a7bf
NicoForlivesi <nicholas.forlivesi@studio.unibo.it>
2026-05-03 17:46:09 +0200

    Ex3: a partial implementation (not working yet)

--- src/main/scala/ex3/Solitaire.scala
+++ src/main/scala/ex3/Solitaire.scala
@@ -1,6 +1,49 @@
 package ex3
 
 object Solitaire extends App:
+  object Position:
+    type Position = (Int, Int)
+    def apply(i: Int, j: Int): Position =
+      require(i >= 0 && j >= 0)
+      (i, j)
+
+  import Position.*
+  type Solution = List[Position]
+
+  object Measure:
+    type Measure = Int
+    def apply(n: Int): Measure =
+      require(n >= 1)
+      n
+
+  import Measure.*
+  case class Board(width: Measure, height: Measure)
+
+  def placeMarks(board: Board): LazyList[Solution] =
+    val start = Position(board.width / 2, board.height / 2)
+
+    def _place(current: Solution): LazyList[Solution] =
+      if current.size == board.width * board.height then LazyList(current)
+      else
+        for
+          next <- validMoves(current.head, current).to(LazyList)
+          solution <- _place(next :: current)
+        yield solution
+
+    def validMoves(pos: Position, current: Solution): List[Position] =
+      val offsets = List((2, 0), (-2, 0), (0, 2), (0, -2), (1, 1), (1, -1), (-1, 1), (-1, -1))
+      for
+        (dx, dy) <- offsets
+        newX = pos._1 + dx
+        newY = pos._2 + dy
+        if newX >= 0 && newX < board.width
+        if newY >= 0 && newY < board.height
+        newPos = (newX, newY)
+        if !current.contains(newPos)
+      yield newPos
+
+    _place(List(start))
+
   def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
     val reversed = solution.reverse
     val rows =
@@ -13,3 +56,8 @@ object Solitaire extends App:
 
 
   println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+
+  @main
+  def printSolutions(): Unit =
+    val solutions = placeMarks(Board(5, 5)).foreach(s =>
+      println("Possible solution: \n" + render(s, 5, 5)))
